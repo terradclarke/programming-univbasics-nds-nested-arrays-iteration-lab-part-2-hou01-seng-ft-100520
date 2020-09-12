f0af7769a35d9d2fef7603ca2d84583c350e9ce8
Terranie Sims <terranie67@gmail.com>
2020-09-12 01:17:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,9 +7,9 @@ row_index = 0
 while row_index < array_1.count do 
   element_index = 0 
   min_in_nested_arrays = 100
-  while element_index < array_1[row_index].count do
-    if array_1[row_index][element_index] < min_in_nested_arrays
-      min_in_nested_arrays = array_1[row_index][element_index]
+  while element_index < src[row_index].count do
+    if src[row_index][element_index] < min_in_nested_arrays
+      min_in_nested_arrays = src[row_index][element_index]
     end 
     element_index += 1 
   end 
